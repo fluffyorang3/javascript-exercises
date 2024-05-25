65efbd9e423f43601a113d0d15521b922f5b51be
fluffyorang3 <fluffyorang3@gmail.com>
2024-05-25 21:07:11 +0200

    Complete 6th exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
+  let answer;
 
-};
+  if (inputYear % 4 == 0 && (inputYear % 100 != 0 || inputYear % 400 == 0)){
+    answer = true;
+  } else {
+    answer = false;
+  }
+  
+  return answer;
+}
 
 // Do not edit below this line
 module.exports = leapYears;

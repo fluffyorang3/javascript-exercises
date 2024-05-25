d4b30556c66e57bc364b4d8a481d8df3751dc4d2
fluffyorang3 <fluffyorang3@gmail.com>
2024-05-25 19:53:18 +0200

    Complete 3rd exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let stringArray = string.split("");
+  let reverseArray = stringArray.reverse();
+  let joinArray = reverseArray.join("");
+  return joinArray;
 };
 
 // Do not edit below this line

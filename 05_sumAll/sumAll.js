baf7e8674bf2ecb737721314da9a4a6d1534dec9
fluffyorang3 <fluffyorang3@gmail.com>
2024-05-25 20:23:17 +0200

    Complete 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let numTerms;
+  
+  if (num1 < 0 || num2 < 0 || typeof num1 != "number" || typeof num2 != "number") {
+    result = "ERROR"
+  } else {
+    num1 < num2 ? numTerms = num2 - num1 + 1:
+    numTerms = num1 - num2 + 1;
+    result = (numTerms / 2) * (num1 + num2);
+  } 
+  
+  return result;
 
 };
 

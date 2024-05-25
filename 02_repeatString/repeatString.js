d2c822eb333dc0b59676a8040f4ed85b17eb0ef0
fluffyorang3 <fluffyorang3@gmail.com>
2024-05-25 19:45:39 +0200

    Complete 2nd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let newString;
+  num < 0 ? newString = "ERROR" :
+  newString = string.repeat(num);
+  return newString;
+  
+  
 };
 
 // Do not edit below this line

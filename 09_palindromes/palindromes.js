508945cc5e89de25235c1296359ed33f3c351914
fluffyorang3 <fluffyorang3@gmail.com>
2024-05-29 22:03:28 +0200

    Solve 9th exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const alphabet = "qwertyuiopasdfghjklzxcvbnm1234567890";
+const alphabetToArray = alphabet.split("");
+const palindromes = function (sentence) {
+  let cleanSentence = sentence.toLowerCase()
+  .split("")
+  .filter(item => alphabetToArray.includes(item))
+  .reverse();
+  
+  if (JSON.stringify(cleanSentence) == JSON.stringify(cleanSentence.reverse())) {
+    return true;
+  } else {
+    return false;
+  };
 };
-
 // Do not edit below this line
 module.exports = palindromes;

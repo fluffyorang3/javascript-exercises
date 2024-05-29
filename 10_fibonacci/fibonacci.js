f73d7fc7eeba1dd731a86468eb88b4aaca896d3d
fluffyorang3 <fluffyorang3@gmail.com>
2024-05-29 23:59:00 +0200

    Advance with 10th exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(nth) {
 
-};
+  if (typeof nth !== 'number') {
+    nth = Number(nth);
+
+  
+  let x = 0;
+  let y = 1;
+  let z = 0;
+  let array = [];
+  for(i = 1; i < (nth); i++) {
+    
+    z = x + y
+    x = y
+    y = z 
+
+    array.push(z);
+  }
+  if (nth == 0) {
+    return 0;
+  } else if (nth > 0){
+    return array[nth - 2];
+  } else {
+    return "OOPS";
+  }
+  
+}
 
 // Do not edit below this line
 module.exports = fibonacci;

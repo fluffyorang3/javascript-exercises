b29b7991f8c5d761568d2e1006ec0beef35e4fa9
fluffyorang3 <fluffyorang3@gmail.com>
2024-05-30 11:24:00 +0200

    Complete 11th exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
 
+
+const getTheTitles = function(book) {
+  let newArray = [];
+  book.forEach((element, index, array) => {
+    newArray.push(element.title);
+  } )
+  return newArray;
 };
 
 // Do not edit below this line
